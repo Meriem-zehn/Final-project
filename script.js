c6928b0be789dab5222f0088e2f78ce67940e320
Meriem-zehn <154067075+Meriem-zehn@users.noreply.github.com>
2024-03-11 12:18:13 +0100

    Update

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("This website uses cookies")
+// const [cartItems, setCartItems] = useState([]);
+// const [totalPrice, setTotalPrice] = useState(0);
+
+// const addToCart = (product) => {
+//     const existingItem = cartItems.find((item) => item.id === product.id);
+//     if (existingItem) {
+//       existingItem.quantity += 1;
+//       setCartItems([...cartItems]);
+//     } else {
+//       const newItem = { ...product, quantity: 1 };
+//       setCartItems([...cartItems, newItem]);
+//     }
+//     setTotalPrice(totalPrice + product.price);
+//   };
+
+
+//   <Product addToCart={addToCart} />
+
+
+
+const card = document.querySelector('.card');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      card.classList.add('visible');
+      observer.disconnect();
+    }
+  });
+});
+
+observer.observe(card);
